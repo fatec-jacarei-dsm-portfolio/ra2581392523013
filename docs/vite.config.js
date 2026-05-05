4ad1ef64b2fac5d6b424fe34d96b47ec50f1c43c
Rafael melo <rafael.pmr@hotmail.com>
2026-05-05 20:49:14 -0300

    fix: base path

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ra2581392523013/", // ← nome exato do repositório da FATEC
+  base: "/ra2581392523013/",
 });
