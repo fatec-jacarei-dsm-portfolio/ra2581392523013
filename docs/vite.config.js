f87dfe7cf115bc60bde3672d3d08f0baa1fbb008
Rafael melo <rafael.pmr@hotmail.com>
2026-05-05 20:55:49 -0300

    fix: base path

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/ra2581392523013/",
-});
+  base: mode === 'production' ? '/ra2581392523028/' : '/',
+  build: {
+    outDir: '.',
+    emptyOutDir: false,
+  }
+}))
