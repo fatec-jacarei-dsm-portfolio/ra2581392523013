c2a3859e435d7d170e08dce4eeebf79da73393dd
Rafael melo <rafael.pmr@hotmail.com>
2026-05-05 21:13:23 -0300

    portfolio rafael prado

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/ra2581392523013/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
